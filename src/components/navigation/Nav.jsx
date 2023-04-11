cd0aac24bd95b8e32bca88ae2ce344678d06e128
nhhasib <nafiul.hasan1997@gmail.com>
2023-04-11 16:29:17 +0600

    First Commit

---
+++ src/components/navigation/Nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Nav = () => {
+    return (
+        <nav className='flex gap-6 p-4'>
+           <Link to="/">Home</Link>
+           <Link to="/statistics">Statistics</Link>
+           <Link to="/AppliedJobs">Applied Jobs</Link>
+           <Link to="/blog">Blog</Link>
+        </nav>
+    );
+};
+
+export default Nav;
---
+++ src/main.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css';
+import Header from './components/header/Header';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Header></Header>,
+  },
+]);
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
