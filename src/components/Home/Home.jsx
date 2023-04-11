fa119135123b08ceec01c667b760b798b8ace81c
nhhasib <nafiul.hasan1997@gmail.com>
2023-04-11 21:41:06 +0600

    Category section added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,11 +3,13 @@ import Header from "../header/Header";
 import { Outlet } from "react-router-dom";
 import "./Home.css";
 import Banner from "../banner/Banner";
+import Categories from "../categories/Categories";
 
 const Home = () => {
   return (
     <div>
     <Banner></Banner>
+    <Categories></Categories>
     </div>
   );
 };
---
+++ src/components/categories/Categories.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import Category from "../category/Category";
+
+const Categories = () => {
+  const [datas, setDatas] = useState([]);
+  useEffect(() => {
+    fetch("/public/categoriesData.json")
+      .then((res) => res.json())
+      .then((datas) => setDatas(datas));
+  }, []);
+
+  return (
+    <div className="w-9/12 mx-auto my-10 text-center">
+      <h1 className="text-4xl font-bold py-4">Job Category List</h1>
+      <p className="pb-6">Explore thousands of job opportunities with all the information you need. Its your future</p>
+      <div className="grid grid-cols-4 my-4 gap-4">
+        {
+        datas.map((data) => (<Category data={data}></Category>))
+        }
+      </div>
+    </div>
+  );
+};
+
+export default Categories;
